ecf28b75c511e32c337b6e16f48c02bd02efed7f
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2021-01-27 00:30:04 +0300

    dashboard page layout

--- src/components/dashboard-page.tsx
+++ src/components/dashboard-page.tsx
@@ -1,5 +1,32 @@
+import LargeMaxWidthLayout from 'components/large-max-width-layout'
+import SiteCurrentYearCopyright from 'components/site-current-year-copyright'
+import SiteHomePageLink from 'components/site-home-page-link'
+import PageLayout from 'components/styled/page-layout'
 import React from 'react'
 
-const DashboardPage: React.FunctionComponent = () => <div>Dashboard Page</div>
+const DashboardPage: React.FunctionComponent = () => {
+
+  const current2021Year = '2021'
+
+  return (
+    <PageLayout>
+      <header>
+        <LargeMaxWidthLayout>
+          <SiteHomePageLink />
+        </LargeMaxWidthLayout>
+      </header>
+      <main>
+        <LargeMaxWidthLayout>
+          DASHBOARD
+        </LargeMaxWidthLayout>
+      </main>
+      <footer>
+        <LargeMaxWidthLayout>
+          <SiteCurrentYearCopyright currentYear={current2021Year} />
+        </LargeMaxWidthLayout>
+      </footer>
+    </PageLayout>
+  )
+}
 
 export default DashboardPage
